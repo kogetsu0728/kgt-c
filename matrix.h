fde73d1e96e208a451d7afc331a08dc5d05bf047
kogetsu <kogetsu0728@gmail.com>
2025-07-10 11:55:28 +0900

    feat: matrix [#1]

---
+++ matrix.h
@@ -0,0 +1,57 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include "vector.h"
+
+typedef struct {
+    Dim row;
+    Vec* vec;
+} MatDat;
+
+typedef MatDat* Mat;
+
+extern const Dim MAX_ROW;
+extern const Dim MAX_COL;
+
+Mat mat_new(const Dim, const Dim);
+Mat mat_del(Mat);
+Mat mat_cpy(const Mat);
+
+Mat vec_to_mat(const Vec);
+
+Dim mat_get_row(const Mat);
+Dim mat_get_col(const Mat);
+
+Sca mat_get_val(const Mat, const Dim, const Dim);
+Sca mat_set_val(Mat, const Dim, const Dim, const Sca);
+
+Mat sca_to_mat(const Sca);
+Mat vec_to_mat(const Vec);
+
+void mat_out(const Vec, FILE*);
+
+Mat mat_add(const Mat, const Mat);
+Mat mat_sub(const Mat, const Mat);
+Mat mat_mul(const Mat, const Mat);
+Mat mat_div(const Mat, const Mat);
+Mat mat_dot(const Mat, const Mat);
+
+Mat mat_add_vec(const Mat, const Vec);
+Mat mat_sub_vec(const Mat, const Vec);
+Mat mat_mul_vec(const Mat, const Vec);
+Mat mat_div_vec(const Mat, const Vec);
+Vec mat_dot_vec(const Mat, const Vec);
+
+Mat mat_add_sca(const Mat, const Sca);
+Mat mat_sub_sca(const Mat, const Sca);
+Mat mat_mul_sca(const Mat, const Sca);
+Mat mat_div_sca(const Mat, const Sca);
+
+bool mat_eq(const Mat, const Mat);
+bool mat_ne(const Mat, const Mat);
+bool mat_gt(const Mat, const Mat);
+bool mat_ge(const Mat, const Mat);
+bool mat_lt(const Mat, const Mat);
+bool mat_le(const Mat, const Mat);
+
+#endif
